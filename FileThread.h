46a4e28650f937012ca071fa3f37bdf308b38f45
SesisAvalos <124007299+SesisAvalos@users.noreply.github.com>
2025-03-30 08:47:32 -0600

    FileThread.h

---
+++ FileThread.h
@@ -0,0 +1,50 @@
+#include <iostream>
+//Se incluye FileThread.h
+#include "FileThread.h"
+
+//Se crea el constructor de la clase FileThread
+FileThread::FileThread() : QThread(){
+}
+
+//Se crea el destructor de la clase FileThread
+FileThread::~FileThread() {
+}
+
+void FileThread::setFile(){file.setFileName(Dir);}
+
+//Esta función guarda los datos de la imagen en un archivo como una matriz. El arreglo tempImage contiene los datos de la imagen en escala de grises 
+void FileThread::saveMatrix(QVector<int> tempImage){
+	if( file.open(QIODevice::WriteOnly))
+	{
+		QTextStream Stream(&file);.
+		for( int i=1;i<19201; i++){
+			Stream<<tempImage[i-1];
+			if(i%160==0) Stream <<"\n";
+			else Stream<<",";
+		}
+	file.close();.
+	}
+}
+
+
+//Se verifica si pythonAI es verdadero o falso
+void FileThread::GetImage()
+{	if(pythonAI){
+		 // Create a QProcess instance to run the Python script
+        QProcess *process = new QProcess(this);
+        QString program = "python";
+        QStringList arguments = {"Hand_predict_Grayscale.py"};
+
+        process->start(program, arguments);     
+	}
+	else{
+		if (!file.exists()) {
+			qDebug() << "Error: File does not exist.";
+		} else {
+			if (!file.remove()) {
+				qDebug() << "Error: Unable to remove the file.";
+			}
+		}
+	} 
+}
+
