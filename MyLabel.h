3d57162f3e4501ef26409bb477cbbb38022fd83c
SesisAvalos <124007299+SesisAvalos@users.noreply.github.com>
2025-04-03 20:16:02 -0600

    MyLabel.h

---
+++ MyLabel.h
@@ -0,0 +1,18 @@
+#ifndef MYLABEL_H
+#define MYLABEL_H
+#include <QtCore>
+#include <QWidget>
+#include <QLabel>
+
+//Este codigo cambia la imagen en un QLabel a traves de un QImage en un entorno multi-hilo
+class MyLabel : public QLabel {
+  Q_OBJECT;
+  public: 
+    MyLabel(QWidget *parent = 0);
+    ~MyLabel();
+
+  public slots:
+    void setImage(QImage);
+    };
+#endif
+
