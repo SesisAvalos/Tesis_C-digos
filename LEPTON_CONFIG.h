badab5281300e489fd14e71f51f2077f8054ae06
SesisAvalos <124007299+SesisAvalos@users.noreply.github.com>
2025-04-22 20:02:10 -0600

    Update LEPTON_CONFIG.h

--- LEPTON_CONFIG.h
+++ LEPTON_CONFIG.h
@@ -20,11 +20,7 @@ void LEP_Connect();
 // Realiza una calibración de corrección de plano uniforme (FFC), que ajusta las diferencias entre píxeles para mejorar la imagen.
 void LEP_PerformFFC();
 
-
-///////////////////////////////////////////////////////////////////////
-////////////////////// Get Config //////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
+//PARA Get Config
 // Obtiene los parámetros radiométricos (como los relacionados con la temperatura).
 void LEP_GetRadParms(LEP_RAD_FLUX_LINEAR_PARAMS_T_PTR Radiometry);
 // Obtiene la configuración actual del modo de ganancia de la cámara.
@@ -34,10 +30,7 @@ void LEP_GetGainConfig(LEP_SYS_GAIN_MODE_OBJ_T_PTR gainModeObj);
 void LEP_GetAGCConfig(LEP_AGC_ROI_T_PTR agcROI, LEP_UINT16 
 *agcHeqClipLimitHigh, LEP_UINT16 *agcHeqDampingFactor, LEP_UINT16 *agcHeqLinearPercent);
 
-///////////////////////////////////////////////////////////////////////
-////////////////////// Basic Config //////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
+//PARA Basic Config 
 //Se controlan y analizan las imágenes térmicas
 void LEP_SetEmissivity(LEP_RAD_FLUX_LINEAR_PARAMS_T Radiometry, float emissivity);
 void LEP_SetAGCEqu(int option);
@@ -49,10 +42,7 @@ void LEP_ROIStatistics(LEP_SYS_SCENE_STATISTICS_T_PTR ROI_Stats, LEP_RAD_SPOTMET
 // Configura la salida de video en formato RAW de 14 bits, que proporciona datos térmicos sin procesar.
 void LEP_SetRaw14();
 
-///////////////////////////////////////////////////////////////////////
-////////////////////// Advanced Config //////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
+//PARA Advanced Config
 // Ajusta parámetros radiométricos avanzados.
 void LEP_SetRadParms(LEP_RAD_FLUX_LINEAR_PARAMS_T Radiometry);
 // Establece configuraciones personalizadas del modo de ganancia.
