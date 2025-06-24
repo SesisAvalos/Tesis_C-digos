c59ccce8b098f663253f56745c7ed1c4a6d77246
SesisAvalos <124007299+SesisAvalos@users.noreply.github.com>
2025-06-23 20:04:54 -0600

    Update LeptonThread.h

--- LeptonThread.h
+++ LeptonThread.h
@@ -1,10 +1,10 @@
-//Se incluyen librerías
+//Se incluyen las librerias 
 #ifndef TEXTTHREAD
 #define TEXTTHREAD
 #include <ctime>
 #include <stdint.h>
 
-//Se incluyen librerías de Qt
+//Se incluye las librerias de Qt
 #include <QThread>
 #include <QtCore>
 #include <QPixmap>
@@ -15,14 +15,15 @@
 #include <QMessageBox>
 #include <QGenericMatrix>
 #include <QVector>
+#include <QTimer>
 
 #include <QPainter>
 #include <QPen>
 
-//Se manda a llamar a LEPTON_CONFIG.h
+//Se manda llamar a LEPTON_CONFIG.h
 #include "LEPTON_CONFIG.h"
 
-//Se definen varias constantes relacionadas con el tamaño de los paquetes de datos
+//Se definen varias constantes relacionadas con el tamanno de los paquetes de datos
 #define PACKET_SIZE 164
 #define PACKET_SIZE_UINT16 (PACKET_SIZE/2)
 #define PACKETS_PER_FRAME 60
@@ -32,11 +33,11 @@
 class LeptonThread : public QThread
 {
   Q_OBJECT;
-//Se declaran métodos públicos
+//Se declaran metodos publicos
 public:
-  LeptonThread();	//Constructor 
-  ~LeptonThread();	//Destructor
-//Se declaran los métodos de configuración
+  LeptonThread(); //Constructor
+  ~LeptonThread(); //Destructor
+//Se declaran los metodos de configuracion
   void setLogLevel(uint16_t);
   void useColormap(int);
   void useLepton(int);
@@ -44,24 +45,24 @@ public:
   void setAutomaticScalingRange();
   void useRangeMinValue(uint16_t);
   void useRangeMaxValue(uint16_t);
-//Se declaran los métodos para el procesamiento y almacenamiento  
-bool Datos(QString,uint16_t,uint16_t);	 
+  //Se declaran los metodos para el procesamiento y almacenamiento
+  bool Datos(QString,uint16_t,uint16_t);
   void saveImage(QString);
   void useOutputFormat(uint8_t);
   void Set_NROI(uint16_t x1, uint16_t x2, uint16_t y1, uint16_t y2);
-  void setBBox(int Temp_BBox[4]);
+  void setBBox(int Temp_BBox[64], int count);
   
-//Método principal para el hilo
+  //Metodo principal para el hilo
   void run();
   
-  //Variables públicas
+  //Variables publicas
   uint16_t Frames;
   uint16_t Time;
   uint16_t FPA_temp;
   uint16_t Hous_temp;
   bool AI;
   
-//Slots públicos
+  //Slots publicos
 public slots:
   void performFFC();
   void Photo();
@@ -69,13 +70,14 @@ public slots:
   void Timed();
   void startAI();
 
-//Señales públicas
+//Sennales publicas
 signals:
   void updateImage(QImage);  
   void infereImage(QVector<int> ImageBuffer);  
-
-//Se declaran las variables privadas
+ 
+ //Se declaran las variables privadas
 private:
+ 
   void log_message(uint16_t, std::string);
   uint16_t loglevel;
   int typeColormap;
@@ -92,20 +94,21 @@ private:
   QImage myImage;
   QString Name;
   bool video;
-//Se declaran las variables de procesamiento de datos, almacenan regiones de interés y estadísticas de temperatura
+  int num_boxes;
+  //Se declaran las variables de procesamiento de datos, almacenpan regiones de interes y estadisticas de temperatura
   void saveImageROI(QString);
   LEP_SYS_VIDEO_ROI_T ROI;
   LEP_RAD_ROI_T ROI_1;
   LEP_SYS_SCENE_STATISTICS_T ROI_Stats;
   LEP_RAD_SPOTMETER_OBJ_KELVIN_T ROI_Stats_1;
   bool AGC,Raw14,Raw16;
-  
-//Se declaran buffers de datos, estos almacenan los paquetes de datos de la cámara antes de ser convertidos en imágenes
-  int array[8];
+
+  //Se declaran buffers de datos, estos almacenan los paquees de datos de la camara antes de ser conertidos en imagenes
+  int array[64];
   uint8_t result[PACKET_SIZE*PACKETS_PER_FRAME];
   uint8_t shelf[4][PACKET_SIZE*PACKETS_PER_FRAME];
   uint16_t *frameBuffer;
+  
 };
 
 #endif
-
